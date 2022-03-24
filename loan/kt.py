3e97207ff7c46d14aa1ead8b182302d8927980ee
Loan <loanvo2107@gmail.com>
2022-03-24 14:32:04 +0700

    xoa

--- loan/kt.py
+++ loan/kt.py
@@ -1,1 +1,13 @@
-print("Nhap")
+print("Nhap So Luong Sinh Vien: ")
+n = int(input())
+ds = {"MSSV":"", "Ho Va Ten":""}
+li = []
+for i in range(1,n+1):
+    print("Nhap MSSV:")
+    m = str(input())
+    ds["MSSV"] = m
+    li.append(ds)
+
+for i in li:
+    print("Danh sach sinh vien la: ")
+    print(i["MSSV"]) 
